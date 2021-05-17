76553c4f350a83500eb2c3a7194725b3bc245825
MalavikaAjit <malavikaajit9@gmail.com>
2021-05-18 01:36:14 +0530

    ADD: added html input form

---
+++ js/input.js
@@ -0,0 +1,21 @@
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function(){
+    output.textContent = salary.value;
+});
+
+//uc1 
+const text = document.querySelector('#text'); 
+const textError = document.querySelector('.text-error'); 
+text.addEventListener('input', function () {
+    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
+    if (nameRegex.test(text.value)) 
+    {
+        textError.textContent = "";
+    }
+    else 
+    {
+        textError.textContent = "Name is Incorrect";
+    }
+});
